7b40caafcb8583dd39d5edcb770a2a4cc65f3c7d
brevin cronk and neha doodi <neha@>
2020-01-13 13:32:08 -0800

    add airthmetic

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = add(number1, number2);
+  alert(result);
+
+  var subtract = function(number1 number2){
+      return number1,number2;
+  };
+  var number1= parseInt(prompt("enter a number"));
+  var number2 = parseInt(prompt("enter another number"));
+  var result3 = add (number1,number2);
+  alert(result3);
+  
+  var multiply = function(number1,number2) {
+    return number1*number2;
+};
+var number1 = parseInt(prompt("enter number1"));
+var number2 = parseInt(prompt("enter another number"));
+var result0 = multiply(number1,number2);
+alert(result0);
+
+
+var divide = function(number1,number2) {
+    return number1/number2;
+};
+var number1 = parseInt(prompt("enter number1"));
+var number2 = parseInt(prompt("enter another number"));
+var result2 = divide (number1, number2);
+alert(result2);
